1c7039a96d91d306e0e8b3b52c64a03f6850b18a
ShyamalV18 <e1397949@u.nus.edu>
2026-01-28 16:37:09 +0800

    Level 0. Renamed as Porus, Greet, Exit

---
+++ src/main/java/Porus.java
@@ -0,0 +1,14 @@
+public class Porus {
+    public static void main(String[] args) {
+
+        String logo =
+                " ____    ___    ____   _   _    ____ \n"
+                        + "|  _ \\  / _ \\  |  _ \\ | | | |  / ___|\n"
+                        + "| |_) || | | | | |_) || | | |  \\___ \\\n"
+                        + "|  __/ | |_| | |  _ < | |_| |   ___) |\n"
+                        + "|_|     \\___/  |_| \\_\\ \\___/   |____/\n";
+
+        System.out.println("Greetings! I'm\n" + logo + " \n" + "How may I assist you today?\n" + "---------------------------------------\n" + "Farewell. Glad to be of service!\n");
+    }
+}
+
