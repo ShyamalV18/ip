52b9bb44102b947c5ae220a342dbd09b1fd70fa6
ShyamalV18 <e1397949@u.nus.edu>
2026-01-29 14:56:56 +0800

    Level 1. Echo

--- src/main/java/Porus.java
+++ src/main/java/Porus.java
@@ -1,6 +1,11 @@
-public class Porus {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Porus {
+
+    private static final String DIVIDER =
+            "--------------------------------------------------";
+
+    public static void main(String[] args) {
         String logo =
                 " ____    ___    ____   _   _    ____ \n"
                         + "|  _ \\  / _ \\  |  _ \\ | | | |  / ___|\n"
@@ -8,7 +13,32 @@ public class Porus {
                         + "|  __/ | |_| | |  _ < | |_| |   ___) |\n"
                         + "|_|     \\___/  |_| \\_\\ \\___/   |____/\n";
 
-        System.out.println("Greetings! I'm\n" + logo + " \n" + "How may I assist you today?\n" + "---------------------------------------\n" + "Farewell. Glad to be of service!\n");
+        printGreeting(logo);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println(DIVIDER);
+                System.out.println("Farewell. Glad to be of service!");
+                System.out.println(DIVIDER);
+                break;
+            }
+
+            System.out.println(DIVIDER);
+            System.out.println("  " + userInput);
+            System.out.println(DIVIDER);
+        }
+    }
+
+    private static void printGreeting(String logo) {
+        System.out.println("Greetings! I'm");
+        System.out.println(logo);
+        System.out.println("How may I assist you today?");
+        System.out.println(DIVIDER);
     }
 }
+
 
